1833d5a9ea3a275257dad523b912f3d136c150b8
Samuel Hawksby-Robinson <samuel@samyoul.com>
2018-01-21 15:54:08 +0000

    Create BlockchainNorthEast.sol

---
+++ contract/BlockchainNorthEast.sol
@@ -0,0 +1,64 @@
+pragma solidity ^0.4.18;
+
+contract BlockchainNorthEast {
+    
+    mapping(address => bool) organisers;
+    mapping(address => bool) attendees;
+    uint128 attendeeCount;
+    
+    function BlockchainNorthEast() public
+    {
+        organisers[msg.sender] = true;
+        attendees[msg.sender] = true;
+        attendeeCount++;
+    }
+    
+    modifier onlyBy()
+    {
+        require(organisers[msg.sender]);
+        _;
+    }
+    
+    modifier onlyByAttendee()
+    {
+        require(attendees[msg.sender]);
+        _;
+    }
+    
+    function getAttendeeCount() public view returns (uint128)
+    {
+        return attendeeCount;
+    }
+    
+    function isAttendee(address _address) public view returns (bool)
+    {
+        return attendees[_address];
+    }
+    
+    function addAttendee(address _attendee) public onlyByAttendee
+    {
+        attendees[_attendee] = true;
+        attendeeCount++;
+    }
+    
+    function removedAttendee(address _attendee) public onlyByAttendee
+    {
+        delete attendees[_attendee];
+        attendeeCount--;
+    }
+    
+    function isOrganiser(address _organiser) public view returns (bool)
+    {
+        return organisers[_organiser];
+    }
+    
+    function addOrganiser(address _organiser) public onlyBy
+    {
+        organisers[_organiser] = true;
+    }
+    
+    function removeOrganiser(address _organiser) public onlyBy
+    {
+        delete organisers[_organiser];
+    }
+}
